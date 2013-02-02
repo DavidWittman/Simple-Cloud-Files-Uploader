b9f2adeeb26a2b8bbec9d75ef97f4b100bba7cfb
David Wittman <david.wittman@rackspace.com>
2013-02-02 13:56:34 -0600

    Version bump to 0.5

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@
 
 from setuptools import setup
 
-VERSION = '0.4'
+VERSION = '0.5'
 REQS = ['python-cloudfiles']
 
 setup(
     name = 'cfupload',
     url = 'https://github.com/DavidWittman/Simple-Cloud-Files-Uploader',
     description = 'A very simple Rackspace Cloud Files uploader',
+    long_description = open('README.md').read(),
     version = VERSION,
     author = 'David Wittman',
     author_email = 'david@wittman.com',
